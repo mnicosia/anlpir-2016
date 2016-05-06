44a4df85cc9e9214a8d0aa217de223abcdd219d2
Massimo Nicosia <m.nicosia@gmail.com>
2016-05-06 16:18:02 +0200

    Comments

--- revtokenizer/src/main/java/it/unitn/revtokenizer/App.java
+++ revtokenizer/src/main/java/it/unitn/revtokenizer/App.java
@@ -14,27 +14,60 @@ import it.unitn.types.ReverseToken;
 public class App {
 	
     public static void main(String[] args) throws UIMAException {
+    	/**
+    	 * We create a new Cas that we will use for our analysis.
+    	 */
     	JCas cas = JCasFactory.createJCas();
-    	
+
+    	/**
+    	 * We put inside the Cas the text that we want to analyze.
+    	 */
     	cas.setDocumentText("The apple is on the table.");
+    	
+    	/**
+    	 * We set the document language.
+    	 * 
+    	 * This step is good practice.
+    	 * 
+    	 * If you set the language an annotator may use this parameter
+    	 * to instantiate a language specific model.
+    	 * 
+    	 * So the effect of this parameter ultimately depends on the
+    	 * implementation of the analsys engine that you add in the pipeline.
+    	 * Many analysis engines have language defaults, or accepts a
+    	 * configuration parameter for selecting the language of the model.
+    	 */
     	cas.setDocumentLanguage("en");
     	
+    	/**
+    	 * We instatiate a segmenter which can perform tokenization.
+    	 */
     	AnalysisEngine segmenter =
     			AnalysisEngineFactory.createEngine(
     					StanfordSegmenter.class);
     	
+    	/**
+    	 * We instatiate our custom analyzer.
+    	 */
     	AnalysisEngine reverseTokenizer =
     			AnalysisEngineFactory.createEngine(
     					ReverseTokenizer.class);
     	
+    	/**
+    	 * We run on the Cas a pipeline constituted by the tokenizer
+    	 * and our custom analyzer.
+    	 */
     	SimplePipeline.runPipeline(cas,
     			segmenter, reverseTokenizer);
     	
+    	/**
+    	 * After the analysis we retrieve our custom annotations and print
+    	 * their values.    	
+    	 */
     	for (ReverseToken token : JCasUtil.select(cas, ReverseToken.class)) {
     		System.out.println(token.getCoveredText() +
     				" " + token.getReversedToken());
     	}
-     
     }
 }
 
--- revtokenizer/src/main/java/it/unitn/revtokenizer/ReverseTokenizer.java
+++ revtokenizer/src/main/java/it/unitn/revtokenizer/ReverseTokenizer.java
@@ -8,24 +8,81 @@ import org.apache.uima.jcas.JCas;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import it.unitn.types.ReverseToken;
 
+/**
+ * TypeCapability is a class level annotation.
+ * 
+ * Type capabilities are useful for specifying the input and output
+ * types of analysis engines.
+ * 
+ * See: https://uima.apache.org/d/uimafit-2.0.0/api/org/apache/uima/fit/descriptor/TypeCapability.html
+ *
+ */
 @TypeCapability(
 	outputs = {"it.unitn.types.ReverseToken"} 
 )
-
+/**
+ * This class implements an analysis engine and it:
+ * 1) extends the JCasAnnotator_ImplBase class
+ * 2) implements the process method which takes the tokens from a Cas and stores additional annotations in it
+ * 
+ * All uimaFit analysis engines implements the JCasAnnotator_ImplBase class.
+ * 
+ * All analysis engines have a process method which must contain the annotation logic.
+ * The Cas containing the current document, and possibly annotations from previous analysis engines in
+ * the pipeline, is passed as input to the analysis engine.
+ * 
+ * The job of the analysis engine is to extract the document text and annotations from the Cas,
+ * to do some processing, and store new annotations in the Cas.
+ * 
+ * Here we are just reversing the strings of the tokens. Please note that it is just a dummy example.
+ * The real take-away is how to create a custom annotator from scratch. 
+ */
 public class ReverseTokenizer extends org.apache.uima.fit.component.JCasAnnotator_ImplBase {
-
+	
 	@Override
 	public void process(JCas cas) throws AnalysisEngineProcessException {
+		/**
+		 * We retrieve and iterate over the tokens contained in the Cas.
+		 * 
+		 * We assume the Cas contains annotations of type Token,
+		 * i.e., a tokenizer was previously run by the pipeline.
+		 * 
+		 * For information about the select function of JCasUtil see:
+		 * http://uima.apache.org/d/uimafit-2.0.0/api/org/apache/uima/fit/util/JCasUtil.html
+		 */
 		for (Token token : JCasUtil.select(cas, Token.class)) {
+			/**
+			 * We retrieve the text spanned by the token.
+			 */
 			String text = token.getCoveredText();
+			
+			/**
+			 * We reverse the string.
+			 */
 			String reversedText = new StringBuilder(text).reverse().toString();
 			
+			/**
+			 * We create a new annotation of type ReverseToken.
+			 */
 			ReverseToken revToken = new ReverseToken(cas);
+			
+			/**
+			 * We set the begin and end offset of the annotation to
+			 * match the properties of the original token.
+			 */
 			revToken.setBegin(token.getBegin());
 			revToken.setEnd(token.getEnd());
+			
+			/**
+			 * We fill the proper field of our annotation with the reversed text.
+			 */
 			revToken.setReversedToken(reversedText);
+			
+			/**
+			 * Important: we call the addToIndexes method which actually adds the
+			 * annotation into the Cas.
+			 */
 			revToken.addToIndexes(cas);
 		}
-	} 
-	  
+	}
 } 
